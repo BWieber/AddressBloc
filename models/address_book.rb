c5c04e147fdefafa830f39af2704810037b77633
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-10-20 18:33:15 -0400

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -23,12 +23,16 @@ require_relative 'entry'
 
 
   def remove_entry(name, phone_number, email)
+      delete_entry = nil
+
       @entries.each do |entry|
 
         if (name == entry.name) && (phone_number == entry.phone_number) && (email == entry.email)
-        @entries.delete(Entry.new(name, phone_number, email))
+        delete_entry = entry
 
         end
       end
+      @entries.delete(delete_entry)
+      p "#{delete_entry} was deleted."
   end
 end
